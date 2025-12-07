1e81f82283d8717cf31fb03ffa0c7e1751cf318f
kedar785 <baiskedar126@gmail.com>
2025-12-07 16:02:59 +0530

    operator condition

---
+++ src/DSA_practice/prac4.java
@@ -0,0 +1,50 @@
+package DSA_practice;
+
+import java.util.*;
+public class prac4 {
+    public static void main(String[] args) {
+
+        //operator condition.
+        Scanner  s=new Scanner(System.in);
+        System.out.println("Enter number ");
+        int n=s.nextInt();
+        int sachin=(n>=0)? 100 : 0;
+        System.out.println(sachin);
+
+        //operator ? such/jhooth
+       // System.out.println((n%2==0)? "Even": "Odd");
+
+
+//        Scanner  sc=new Scanner(System.in);
+//        System.out.println("first");
+//        int a=sc.nextInt();
+//        System.out.println("second");
+//        int b=sc.nextInt();
+//        System.out.println("third");
+//        int c=sc.nextInt();
+//
+//        if(a>b &&a>c) System.out.println("largest 'a'");
+//        else if(b>a &&b>c) System.out.println("largest 'b'");
+//        else System.out.println("largest 'c'");
+
+//        System.out.println("Enter first num");
+//        int a=sc.nextInt();
+//        System.out.println("Enter second num");
+//        int b=sc.nextInt();
+//        System.out.println("Enter third num");
+//        int c=sc.nextInt();
+//        if(a+b>c && b+c>a && a+c>b) System.out.println("side of a triangle");
+//        else System.out.println("NOT a side of triangle");
+
+      //  System.out.println("Enter the number :");
+        //int n=sc.nextInt();
+
+//        if(n>99  && n<1000){
+//            System.out.println("three digit no");
+//        }else System.out.println("not a three digit no");
+
+//        if(n%5==0  || n%3==0){
+//            System.out.println("number is a divisible");
+//        }else System.out.println("number is a not divisible");
+    }
+}
