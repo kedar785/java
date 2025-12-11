b5d6668d12ee9527e576f49cd06d05e041a43413
kedar785 <baiskedar126@gmail.com>
2025-12-11 22:41:54 +0530

    loop

---
+++ src/DSA_practice/prac5.java
@@ -0,0 +1,39 @@
+package DSA_practice;
+
+//Loop
+import java.util.Scanner;
+public class prac5 {
+    public static void main(String[] args) {
+//        for(int i=0;i<10;i++){
+//            System.out.println("i love u");
+//        }
+//        for (int i=1;i<=10;i++){
+//            System.out.print(17*i+" ");
+//        }
+//        for (int i = 1; i < 100; i++) {
+//            if(i%2!=0 && i%3==0){
+//                System.out.println(i+" ");
+//            }
+//        }
+//        System.out.println("Enter no");
+//        Scanner sc=new Scanner(System.in);
+//        int n=sc.nextInt();
+//for(int i=2;i<=3*n-1;i+=3){
+//    System.out.println(i+" ")
+//    ;
+//        for(int i=99;i>0;i-=4){
+//            System.out.println(i+" ");
+//}
+//        int a=1,r=2;
+//        for(int i=0;i<=n;i++){
+//            System.out.println(a);
+//            a=a*r;
+//
+//        }
+
+        //ASCI
+        for(int i=65;i<=90;i++){
+            System.out.println((char) i +"--"+i);
+        }
+    }
+}
