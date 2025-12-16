3982f066195d80a3c55f52c9853295c4a03dfda9
kedar785 <baiskedar126@gmail.com>
2025-12-16 21:36:45 +0530

    o1 and on

---
+++ src/time_space_compexity/big_o1.java
@@ -0,0 +1,12 @@
+package time_space_compexity;
+
+public class big_o1 {
+  static int getFirstElement(int[] arr){
+      return arr[0];
+  }
+
+    public static void main(String[] args) {
+        int[] arr={1,2,3,4};
+        System.out.println(getFirstElement(arr));
+    }
+}
---
+++ src/time_space_compexity/big_on.java
@@ -0,0 +1,14 @@
+package time_space_compexity;
+
+public class big_on {
+    static void printElements(int[] arr){
+        for(int i=0;i<arr.length; i++){
+            System.out.println(arr[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+        int [] arr={1,2,3,4,5};
+        printElements(arr);
+    }
+}
