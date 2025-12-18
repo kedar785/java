4616eb87875155b610005eb90fc0c8b46b47a788
kedar785 <baiskedar126@gmail.com>
2025-12-18 20:39:51 +0530

    BinaryTrangle

--- src/basicjava/color.java
+++ src/basicjava/color.java
@@ -1,4 +1,27 @@
 package basicjava;
 
+import java.util.*;
 public class color {
-}
+    public static void main(String[] args) {}}
+//        int num{}
+//        int arr;
+//        int mid;
+//        int low;
+//          int h;
+//        int h=arrlength-1;
+//        while(mid<=h){
+//            if(arr[mid]==0){
+//                swap(arr,low,mid);
+//                low++;
+//                mid++;
+//            }
+//            else if(arr [mid]==1){
+//                mid++;
+//            }
+//            else{
+//                swap(arr,mid,h);
+//                h-1;
+//            }
+//        }
+//    }
+//}
--- src/pattern/rectangle.java
+++ src/pattern/rectangle.java
@@ -1,4 +1,30 @@
 package pattern;
 
+import java.util.*;
 public class rectangle {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int m=sc.nextInt();
+        int n = sc.nextInt();
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+
+                if(i==(m+1)/2  || j==(n+1)/2){
+                    System.out.print("* ");
+
+                }
+                else{
+                    System.out.print("  ");
+                }
+
+//                if(i==1 || i==m  || j==1 || j==n){
+//                System.out.print("* ");
+//                }
+//                else {
+//                    System.out.print("  ");
+//                }
+            }
+            System.out.println();
+        }
+    }
 }
