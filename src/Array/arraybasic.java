1b410b038568340ae556f708e7b6120085b557a3
kedar785 <baiskedar126@gmail.com>
2025-12-21 15:02:56 +0530

    arr basic

---
+++ src/Array/arraybasic.java
@@ -0,0 +1,17 @@
+package Array;
+
+public class arraybasic {
+    public static void main(String[] args) {
+        int[] x={5,6,34,56,3,5};
+        //indexing
+        System.out.println(x[3]);//access
+        //updating  mutability
+        x[3]=88;
+        System.out.println(x[3]);
+      int []arr=new int[4];  //4 size ka arr 0 to 3 index
+        arr[0]=10;
+        arr[1]=23;
+        arr[2]=24;
+        arr[3]=7;
+    }
+}
