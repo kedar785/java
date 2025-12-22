0455178d4a07869c19322cd66d1904ae9e59f7a9
kedar785 <baiskedar126@gmail.com>
2025-12-22 22:01:18 +0530

    pass Arr

--- src/Array/passingArraytofunandMethod.java
+++ src/Array/passingArraytofunandMethod.java
@@ -2,8 +2,16 @@ package Array;
 
 public class passingArraytofunandMethod {
     public static void main(String[] args) {
-      int x=9;
+        int x[] = {1, 3, 4, 5};
+        System.out.println(x[2]);
+        change(x);
+        System.out.println(x[2]);
+    }
+
+    public static void change(int[]y) {
+        y[2]=99;
+    }
 
     }
 
-}
+
