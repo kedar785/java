9390c4b8a19f7e52ec8d07f6836e481fbe74c08f
kedar785 <baiskedar126@gmail.com>
2025-12-24 18:46:32 +0530

    for Each loop

---
+++ src/Array/forEachLoop.java
@@ -0,0 +1,10 @@
+package Array;
+
+public class forEachLoop {
+    public static void main(String[] args) {
+        int [] arr={2,4,5,6,7};
+        for(int ele : arr){
+            System.out.print(ele+" ");
+        }
+    }
+}
