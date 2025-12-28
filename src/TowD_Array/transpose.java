dd2c56fb999ec08ec3592232abd7160b70462ee6
kedar785 <baiskedar126@gmail.com>
2025-12-28 19:01:36 +0530

    trans pose

---
+++ src/TowD_Array/transpose.java
@@ -0,0 +1,31 @@
+package TowD_Array;
+
+import java.util.*;
+public class transpose {
+    public static void main(String[] args) {
+        int[][] arr = {{1, 2, 3, 4}, {5, 4, 3, 2}, {6, 7, 8, 3}, {9, 3, 6, 8}};
+        print(arr);
+        //Transport
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < i; j++) {
+                int temp = arr[i][j];
+                arr[i][j] = arr[j][i];
+                arr[j][i] = temp;
+            }
+        }
+        print(arr);
+    }
+
+    private static void print(int[][] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+
+    
+System.out.println();
+}
+}
