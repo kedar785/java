703cfaaede9265c23c02e271531cddad26e4887a
kedar785 <baiskedar126@gmail.com>
2026-01-06 21:21:42 +0530

    string me input lena

---
+++ src/strings/stringBasic.java
@@ -0,0 +1,14 @@
+package strings;
+
+import java.util.*;
+public class stringBasic {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        System.out.println(s);
+        String t=sc.next();
+        System.out.println(t);
+//        String str="i am a good boy";
+//        System.out.println(str);
+    }
+}
