4f26b3c11881531f9ec024d36170cbb3acfbb295
kedar785 <baiskedar126@gmail.com>
2026-01-08 20:08:55 +0530

    Builder mwthods

---
+++ src/strings/BuilderMethod.java
@@ -0,0 +1,20 @@
+package strings;
+
+import java.util.Locale;
+
+public class BuilderMethod {
+    public static void main(String[] args) {
+//        String s=" KervbBhfDDGFC";
+//        System.out.println(s.toLowerCase()); //sbhi later small ho jayege.
+//        System.out.println(s.toUpperCase()); //sabhi later capital ho jayege.
+
+//        String x="harshita";
+//        System.out.println(x.contains("harsh"));
+//        if(x.contains("arsh")) System.out.println("happy");
+//        System.out.println(x.startsWith("ha"));
+//        System.out.println(x.endsWith("ita"));
+        String a="harmeet";
+        String b="harneet";
+        System.out.println(a.compareTo(b));
+    }
+}
