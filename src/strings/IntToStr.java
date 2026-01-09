c98593e7863c6b9f0f3ab5a978bbcb0dc628890f
kedar785 <baiskedar126@gmail.com>
2026-01-09 16:02:12 +0530

    n by n ki Q

---
+++ src/strings/IntToStr.java
@@ -0,0 +1,16 @@
+package strings;
+
+import java.util.Scanner;
+
+public class IntToStr {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+//        String s="";//empty string
+//        s+=n;
+        String s=Integer.toString(n);
+      System.out.println(s);
+
+
+    }
+}
