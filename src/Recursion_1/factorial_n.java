9513b5bd84477e0a638ecb9968728cc91610a6e4
kedar785 <baiskedar126@gmail.com>
2026-01-17 21:55:47 +0530

    factorial

---
+++ src/Recursion_1/factorial_n.java
@@ -0,0 +1,12 @@
+package Recursion_1;
+
+public class factorial_n {
+    public static void main(String[] args) {
+
+        System.out.println(fact(5));
+    }
+    public static int fact(int n) {
+        if(n==0 ||n==1) return 1;
+        return n*fact(n-1);
+    }
+}
