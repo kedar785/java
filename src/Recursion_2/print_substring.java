0b3bde9387698786739ea6fd4a08eae6f39e1dee
kedar785 <baiskedar126@gmail.com>
2026-01-21 19:52:05 +0530

    recursion substring

---
+++ src/Recursion_2/print_substring.java
@@ -0,0 +1,24 @@
+package Recursion_2;
+
+
+import java.util.*;
+
+public class print_substring {
+    public static void main(String[] args) {
+        String s="abc";
+
+        List<String>  list= new  ArrayList<>();
+        subset(" ",s,0,list);
+        System.out.println(list);
+    }
+    private static void subset(String ans,String s,int idx, List<String>  list ){
+        if(idx==s.length()){
+          if(ans.length()!=0)  list.add(ans);
+            return ;
+
+        }
+        char ch=s.charAt(idx);
+        subset(ans+ch,s,idx+1,list);  //pick
+        subset(ans,s,idx+1,list);    //skip
+    }
+}
