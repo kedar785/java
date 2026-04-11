d6ff48ffb20c012ff517389c5e2ff25ca721c007
kedar785 <baiskedar126@gmail.com>
2026-04-11 20:29:13 +0530

    display

--- src/LINK_LIST1/linklistDatastructure.java
+++ src/LINK_LIST1/linklistDatastructure.java
@@ -7,10 +7,48 @@ class Node {  //usre difine data type
         this.val = val;
     }
 }
-class LinkList{
+class LinkList {
     Node head; //null
     Node tail; //null
-    void addAtTail(int val){
+    int size;
+
+    void addAtHead(int val) {
+        Node temp = new Node(val);
+        if (head == null) head = tail = temp;
+        else {
+            temp.next = head;
+            head = temp;
+        }
+        size++;
+    }
+
+    void addAtTail(int val) {
+        Node temp = new Node(val);
+        if (tail == null) head = tail = temp;
+        else {
+            tail.next = temp;
+
+            tail = temp;
+        }
+        size++;
+    }
+
+    void deleteAtHead(int val) {
+        if (head == null) {
+            System.out.println("list is empty");
+            return;
+        }
+        head = head.next;
+        if (head == null) tail = null;
+        size--;
+    }
+    void display(){
+        if(head==null) return;
+        Node temp=head;
+        while(temp!=null){
+            System.out.print(temp.val+"->");
+            temp=temp.next;
+        }
 
     }
 }
@@ -18,5 +56,13 @@ public class linklistDatastructure {
     public static void main(String[] args) {
         LinkList ll=new LinkList();
         ll.addAtTail(10);
+        ll.addAtTail(20);
+        ll.addAtTail(50);
+        ll.addAtHead(70);
+        ll.addAtHead(80);
+        ll.deleteAtHead(80);
+
+        ll.display();
+        System.out.println( "   size of link list : "+ ll.size);
     }
 }
---
+++ src/cls_recursion/pldrm_recursn.java
@@ -0,0 +1,5 @@
+package cls_recursion;
+
+public class pldrm_recursn {
+    //recursion se palindrome check .
+}
