1f4d656725aa99d5f572e8aaf72889c332d5745f
kedar785 <baiskedar126@gmail.com>
2026-04-11 20:39:49 +0530

    palindom inrecursion

--- src/cls_recursion/pldrm_recursn.java
+++ src/cls_recursion/pldrm_recursn.java
@@ -2,4 +2,20 @@ package cls_recursion;
 
 public class pldrm_recursn {
     //recursion se palindrome check .
-}
+        static boolean isPalindrome(String s) {
+            return check(s, 0, s.length() - 1);
+        }
+
+        static boolean check(String s, int i, int j) {
+            if (i >= j) return true;              // base case
+            if (s.charAt(i) != s.charAt(j)) return false; // mismatch
+            return check(s, i + 1, j - 1);        // recursion
+        }
+
+        public static void main(String[] args) {
+            System.out.println(isPalindrome("madam"));  // true
+            System.out.println(isPalindrome("hello"));  // false
+        }
+    }
+
+
