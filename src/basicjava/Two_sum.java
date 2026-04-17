b58d6064b23113a55b220487ec240732f516f6b4
kedar785 <baiskedar126@gmail.com>
2026-04-17 21:46:01 +0530

    two sum

---
+++ src/basicjava/Two_sum.java
@@ -0,0 +1,32 @@
+package basicjava;
+import java.util.HashMap;
+public class Two_sum {
+    public static int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int diff = target - nums[i];
+
+            if (map.containsKey(diff)) {
+                return new int[]{map.get(diff), i};
+            }
+
+            map.put(nums[i], i);
+        }
+        return new int[]{};
+    }
+
+    // Main method to run the program
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+
+        int[] result = twoSum(nums, target);
+
+        if (result.length == 2) {
+            System.out.println("Indices: " + result[0] + ", " + result[1]);
+        } else {
+            System.out.println("No solution found!");
+        }
+    }
+}
