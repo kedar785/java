372df223130160dac75d4948023d03eaa2457d0d
kedar785 <baiskedar126@gmail.com>
2026-05-26 20:34:35 +0530

    dp gred

---
+++ src/DP/NumberOfPath.java
@@ -0,0 +1,22 @@
+package DP;
+
+public class NumberOfPath {
+    public int numberOfPath(int m,int n){
+        int [][] dp=new int[m][n];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 || j==0) dp[i][j]= 1;
+                else dp[i][j]= dp[i-1][j]+dp[i][j-1];
+            }
+
+        }
+        return dp[m-1][n-1];
+//        int [][] dp=new int[m+1][n+1];
+//        return paths(m,n,dp);
+    }
+//    public int paths(int m, int n,int[][]dp){
+//        if(m==1 ||n==1) return 1;
+//        if(dp[m][n]!=0) return dp[m][n];
+//        return dp[m][n]=paths(m-1,n,dp)+paths(m,n-1,dp);
+//    }
+}
