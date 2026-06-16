d2b4b4a8bfa16d9f20c6744d514d53baa6303e4e
kedar785 <baiskedar126@gmail.com>
2026-06-16 19:59:05 +0530

    rotation

--- src/Array/leetCodeArrRotation.java
+++ src/Array/leetCodeArrRotation.java
@@ -12,13 +12,11 @@ public class leetCodeArrRotation {
             reverse(nums, 0, k - 1);
             reverse(nums, k, n - 1);
         }
-
         private void reverse(int[] nums, int left, int right) {
             while(left < right) {
                 int temp = nums[left];
                 nums[left] = nums[right];
                 nums[right] = temp;
-
                 left++;
                 right--;
             }
