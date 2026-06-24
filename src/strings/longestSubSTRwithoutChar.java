1eff639917da9b1661a700250594642787f95801
kedar785 <baiskedar126@gmail.com>
2026-06-24 19:42:05 +0530

    sub str

---
+++ src/strings/longestSubSTRwithoutChar.java
@@ -0,0 +1,23 @@
+package strings;
+import java.util.HashMap;
+public class longestSubSTRwithoutChar {
+    public int lengthOfLongestSubstring(String s) {
+            HashMap<Character, Integer> map = new HashMap<>();
+            int left = 0;
+            int maxLen = 0;
+
+            for (int right = 0; right < s.length(); right++) {
+                char ch = s.charAt(right);
+
+                if (map.containsKey(ch) && map.get(ch) >= left) {
+                    left = map.get(ch) + 1;
+                }
+
+                map.put(ch, right);
+                maxLen = Math.max(maxLen, right - left + 1);
+            }
+
+            return maxLen;
+        }
+    }
+
