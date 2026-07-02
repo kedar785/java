2b153d229c35281ae2ac570a7c60224907e9c93e
kedar785 <baiskedar126@gmail.com>
2026-07-02 20:29:57 +0530

    build arr in stack

---
+++ src/Array/BuildAnArrWithStackOperation.java
@@ -0,0 +1,19 @@
+package Array;
+
+import java.util.*;
+public class BuildAnArrWithStackOperation {
+    public List<String> buildArray(int[] target, int n) {
+        List<String> op = new ArrayList<>();
+        int i=1;
+        for(int num : target){
+            while(i < num){
+                op.add("Push");
+                op.add("Pop");
+                i++;
+            }
+            op.add("Push");
+            i++;
+        }
+        return op;
+    }
+}
