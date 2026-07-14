718822323fd1a09b2b1dc16fe06c1e30105f2d4b
kedar785 <baiskedar126@gmail.com>
2026-07-14 16:34:56 +0530

    partition

---
+++ src/Array/ArrPartition.java
@@ -0,0 +1,12 @@
+package Array;
+import java.util.*;
+public class ArrPartition {
+    public int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int count =0;
+        for(int i=0;i<nums.length;i=i+2){
+            count +=nums[i];
+        }
+        return count;
+    }
+}
